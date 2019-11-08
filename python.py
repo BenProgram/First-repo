10ecb332f6121d067df0b3c7875960e15c03231d
Benjamin malunda <bkasongo3asm7@gmail.com>
2019-11-08 13:53:35 +0200

    Employees

--- python.py
+++ python.py
@@ -1,1 +1,15 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@injection.com'
 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('kasongo', 'Freddy', 6000)
+emp_2 = Employee('benjamin', 'malunda', 50000)
+
+print(emp_1.fullname())
